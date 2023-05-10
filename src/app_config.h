3ea39a5a0a08e2dab5342797b32cc219cc51b70c
al-martyn1 <amart@mail.ru>
2023-05-11 02:40:11 +0300

    Добавил забытый app_config.h

---
+++ src/app_config.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <string>
+#include <vector>
+#include <map>
+
+#include "umba/program_location.h"
+#include "umba/enum_helpers.h"
+#include "umba/flag_helpers.h"
+
+#include "umba/regex_helpers.h"
+
+//----------------------------------------------------------------------------
+
+
+
+struct FilenamePair
+{
+    std::string inputFilename ;
+    std::string outputFilename;
+};
+
+//----------------------------------------------------------------------------
+struct AppConfig
+{
+
+    bool                                     allFiles = false;
+    bool                                     scanMode = false;
+    std::vector<std::string>                 scanPaths;
+    std::vector<std::string>                 includeFilesMaskList;
+    std::vector<std::string>                 excludeFilesMaskList;
+
+}; // struct AppConfig
+
+
+
+
