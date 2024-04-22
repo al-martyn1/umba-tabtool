7c96cd507d765e6e71600121155229d23ad281e2
Alexandr Martynov <mag@unicont.com>
2024-04-22 12:01:08 +0300

    Фикусанул клонирование либ

--- _libs/_clone_libs.bat
+++ _libs/_clone_libs.bat
@@ -26,7 +26,7 @@ set PREFIX=git@%BASE%:%ACC%
 :DO_CLONE
 
 git clone %PREFIX%/umba_mm_mod_encodings.git   %~dp0\encoding
-git clone %PREFIX%/marty_cpp.git
+git clone %PREFIX%/marty_cpp.git               %~dp0\marty_cpp
 git clone %PREFIX%/umba_mm_mod_sfmt.git        %~dp0\sfmt
 git clone %PREFIX%/umba_mm_mod_umba.git        %~dp0\umba
 
